eec06f4856b35ff490d6cc6923a5adf6db45fba8
Marilyn Oosthuizen <marilyntjie@outlook.com>
2022-04-02 14:37:18 +0200

    2 April Updates

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const { engine } = require("express/lib/application");
 
 const app = express();
 
@@ -7,16 +9,21 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
+app.set("view engine", "ejs");
+
 const toDoList = [];
+const date = new Date().getFullYear();
+const currentDay = new Date().toLocaleDateString();
+
 
 app.get("/", (req, res)=>{
-    res.sendFile(__dirname + "/index.html");
+    res.render("home.ejs", {list: toDoList, currentYear: date, today: currentDay});
 });
 
 app.post("/", (req, res)=>{
     const textInput = req.body.addItem;
     toDoList.push(textInput);
-    console.log(toDoList);
+    res.redirect("/");
 });
 
 app.listen(3000, console.log("Server running on port 3000"));
